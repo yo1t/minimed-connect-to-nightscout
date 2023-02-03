c5dfcf3c2974c57d12b8edd2827d0e3703c2f70c
Yoichi Takizawa <yo1@tkywave.com>
2023-02-03 22:42:45 +0900

    Update carelink.js

--- carelink.js
+++ carelink.js
@@ -17,7 +17,7 @@ var MMCONNECT_SERVERNAME = process.env['MMCONNECT_SERVERNAME'];
 var DEFAULT_CARELINKSERVERADDRESS = MMCONNECT_SERVERNAME || (CARELINK_EU ? "carelink.minimed.eu" : "carelink.minimed.com");
 
 var DEFAULT_COUNTRYCODE = process.env['MMCONNECT_COUNTRYCODE'] || 'jp';
-var DEFAULT_LANGCODE = process.env['MMCONNECT_LANGCODE'] || '';
+var DEFAULT_LANGCODE = process.env['MMCONNECT_LANGCODE'] || 'en';
 
 
 var Client = exports.Client = function (options) {
